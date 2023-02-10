a7497c65b9bd41fe1e54daf512bbcdc38b3ee35f
LeoAndo <leo.777.info@gmail.com>
2023-02-10 18:20:35 +0900

    uistateの仕組みを導入する

--- lib/data/repository/github_repo_repository_impl.dart
+++ lib/data/repository/github_repo_repository_impl.dart
@@ -1,17 +1,27 @@
+// Package imports:
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 // Project imports:
 import 'package:flutter_github_search/data/api/github/github_api.dart';
 import 'package:flutter_github_search/data/api/github/response/search_repositories/search_repositories_response.dart';
 import 'package:flutter_github_search/domain/model/repository_summary.dart';
 import '../../domain/repository/github_repo_repository.dart';
 
+final Provider githubRepoRepositoryProvider =
+    Provider<GithubRepoRepository>((ref) {
+  final api = ref.watch(githubApiProvider);
+  return GithubRepoRepositoryImpl(api);
+});
+
 class GithubRepoRepositoryImpl implements GithubRepoRepository {
-  GithubRepoRepositoryImpl();
-  final GithubApi api = GithubApi();
+  final GithubApi _api;
+  GithubRepoRepositoryImpl(this._api);
 
   @override
   Future<List<RepositorySummary>> searchRepositories(
       {required String query, required int page, required int perPage}) async {
-    final ret = await api.searchRepositories(
+    // final api = _ref.read(githubApiProvider);
+    final ret = await _api.searchRepositories(
         query: query, page: page, perPage: perPage);
     return ret.toModels();
   }
