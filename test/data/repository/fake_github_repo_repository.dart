caf9fcdf6be53efdd187fcef0fdd3db2a2a15f25
LeoAndo <leo.777.info@gmail.com>
2023-02-15 10:31:28 +0900

    Widget Test追加(DetailScreen)

---
+++ test/data/repository/fake_github_repo_repository.dart
@@ -0,0 +1,35 @@
+// Project imports:
+import 'package:flutter_github_search/domain/exception/api_exceptions.dart';
+import 'package:flutter_github_search/domain/model/repository_detail.dart';
+import 'package:flutter_github_search/domain/model/repository_summary.dart';
+import 'package:flutter_github_search/domain/repository/github_repo_repository.dart';
+
+class FakeGithubRepoRepository implements GithubRepoRepository {
+  FakeGithubRepoRepository({required this.isSuccess});
+  static const ownerName = "flutter";
+  static const repositoryName = "flutter";
+  static const successData = RepositoryDetail(
+    name: "flutter",
+    ownerAvatarUrl: "https://avatars.githubusercontent.com/u/14101776?v=4",
+    stargazersCount: 147731,
+    forksCount: 24075,
+    openIssuesCount: 11390,
+    watchersCount: 3561,
+    language: "Dart",
+    description: "Hello, Flutter",
+  );
+  final bool isSuccess;
+  static const errorMessage = 'network error';
+  static const ApiException errorData = NetworkException(errorMessage);
+  @override
+  Future<List<RepositorySummary>> searchRepositories(
+      {required String query, required int page}) async {
+    return [];
+  }
+
+  @override
+  Future<RepositoryDetail> fetchRepositoryDetail(
+      {required String ownerName, required String repositoryName}) async {
+    return (isSuccess) ? successData : throw errorData;
+  }
+}
